5762952c4d996584160e3f9900588fb0d348fb4f
Alokhas48 <154874661+Alokhas48@users.noreply.github.com>
2024-02-08 07:26:32 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -2,35 +2,39 @@
 from Adarsh.bot import StreamBot
 from Adarsh.vars import Var
 import logging
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from pyrogram.errors import UserNotParticipant
-from Adarsh.utils.database import Database
-from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
-from Adarsh.utils.human_readable import humanbytes
-
 logger = logging.getLogger(__name__)
+from Adarsh.bot.plugins.stream import MY_PASS
+from Adarsh.utils.human_readable import humanbytes
+from Adarsh.utils.database import Database
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
+from pyrogram.types import ReplyKeyboardMarkup
 
-if Var.MY_PASS:
-    buttonz = ReplyKeyboardMarkup(
-        [
-            ["start⚡️", "help📚", "DC"],
-            ["ping📡", "status📊"]
-        ],
-        resize_keyboard=True
-    )
+if MY_PASS:
+            buttonz=ReplyKeyboardMarkup(
+            [
+                ["start⚡️","help📚","DC"],
+                ["ping📡","status📊"]
+                        
+            ],
+            resize_keyboard=True
+        )
 else:
-    buttonz = ReplyKeyboardMarkup(
-        [
-            ["start⚡️", "help📚", "DC"],
-            ["ping📡", "status📊"]
-        ],
-        resize_keyboard=True
-    )
+            buttonz=ReplyKeyboardMarkup(
+            [
+                ["start⚡️","help📚","DC"],
+                ["ping📡","status📊"]
+                        
+            ],
+            resize_keyboard=True
+        )
 
-
-@StreamBot.on_message((filters.command("start") | filters.regex('start⚡️')) & filters.private)
+            
+            
+@StreamBot.on_message((filters.command("start") | filters.regex('start⚡️')) & filters.private )
 async def start(b, m):
     if not await db.is_user_exist(m.from_user.id):
         await db.add_user(m.from_user.id)
@@ -49,7 +53,7 @@ async def start(b, m):
                 )
                 return
         except UserNotParticipant:
-            await StreamBot.send_photo(
+             await StreamBot.send_photo(
                 chat_id=m.chat.id,
                 photo="https://graph.org/file/57bb7cd0a8566bd93f3df.jpg",
                 caption="<i>𝙹𝙾𝙸𝙽 CHANNEL 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴🔐</i>",
@@ -60,24 +64,24 @@ async def start(b, m):
                         ]
                     ]
                 ),
+                
             )
-            return
+             return
         except Exception:
             await b.send_message(
                 chat_id=m.chat.id,
                 text="<i>𝓢𝓸𝓶𝓮𝓽𝓱𝓲𝓷𝓰 𝔀𝓮𝓷𝓽 𝔀𝓻𝓸𝓷𝓰</i> <b> <a href='https://t.me/Discuss_Linux'>CLICK HERE FOR SUPPORT </a></b>",
-                disable_web_page_preview=True
-            )
+                
+                disable_web_page_preview=True)
             return
     await StreamBot.send_photo(
         chat_id=m.chat.id,
-        photo="https://graph.org/file/6f07d643cbcfa5b3ccffb.jpg",
-        caption=f'Hi {m.from_user.mention(style="md")}!,\nI am Telegram File to Link Generator Bot with Channel support.\nSend me any file and get a direct download link and streamable link.!',
-        reply_markup=buttonz
-    )
+        photo ="https://graph.org/file/6f07d643cbcfa5b3ccffb.jpg",
+        caption =f'Hi {m.from_user.mention(style="md")}!,\nI am Telegram File to Link Generator Bot with Channel support.\nSend me any file and get a direct download link and streamable link.!',
+        reply_markup=buttonz)
 
 
-@StreamBot.on_message((filters.command("help") | filters.regex('help📚')) & filters.private)
+@StreamBot.on_message((filters.command("help") | filters.regex('help📚')) & filters.private )
 async def help_handler(bot, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id)
@@ -92,6 +96,7 @@ async def help_handler(bot, message):
                 await bot.send_message(
                     chat_id=message.chat.id,
                     text="<i>Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ FROM USING ᴍᴇ. Cᴏɴᴛᴀᴄᴛ ᴛʜᴇ Dᴇᴠᴇʟᴏᴘᴇʀ</i>",
+                    
                     disable_web_page_preview=True
                 )
                 return
@@ -103,4 +108,28 @@ async def help_handler(bot, message):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("🤖 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url
+                            InlineKeyboardButton("🤖 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                
+            )
+            return
+        except Exception:
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ [Alok](https://t.me/Prime_Alok).",
+                disable_web_page_preview=True)
+            return
+    await message.reply_text(
+        text="""<b> Send me any file or video i will give you streamable link and download link.</b>\n
+<b> I also support Channels, add me to you Channel and send any media files and see miracle✨ also send /list to know all commands""",
+        
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("Channel", url="https://t.me/Linux_Bots")],
+                [InlineKeyboardButton("Developer", url="https://t.me/Prime_Alok")]
+            ]
+        )
+                                          )
